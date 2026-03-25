1b8ff630a6af21c497bc029791d0541764d87885
ELMALMIMOHAMMED <elmalmisimo122@gmail.com>
2026-03-25 09:50:44 +0100

    Create SessionServices.java

---
+++ services/SessionServices.java
@@ -0,0 +1,52 @@
+package services;
+
+public class SessionService {
+
+    private static int currentUserId = -1;
+    private static String currentEmail;
+    private static String currentToken;
+
+    // =========================
+    // SET USER
+    // =========================
+    public static void setCurrentUser(int userId, String email) {
+        currentUserId = userId;
+        currentEmail = email;
+
+        System.out.println("✅ USER SET -> ID: " + userId + " | EMAIL: " + email);
+    }
+
+    // =========================
+    // GET USER ID
+    // =========================
+    public static int getCurrentUserId() {
+        return currentUserId;
+    }
+
+    // =========================
+    // TOKEN
+    // =========================
+    public static void setCurrentToken(String token) {
+        currentToken = token;
+    }
+
+    public static String getCurrentToken() {
+        return currentToken;
+    }
+
+    // =========================
+    // LOGIN CHECK
+    // =========================
+    public static boolean isLoggedIn() {
+        return currentUserId > 0;
+    }
+
+    // =========================
+    // LOGOUT
+    // =========================
+    public static void logout() {
+        currentUserId = -1;
+        currentEmail = null;
+        currentToken = null;
+    }
+}
